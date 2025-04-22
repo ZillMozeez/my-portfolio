6b1710adf08eda4a252d9235114e71f07a17cd52
ZillMozeez <55386900+ZillMozeez@users.noreply.github.com>
2025-04-22 22:59:25 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// === TYPING ANIMATION
+// var typed = new Typed(".typing", {
+//     strings:["Web Developer", "YouTuber"],
+//     typeSpeed:100,
+//     BackSpeed:60,
+//     loop:true
+// });
+
+// === FORM VALIDATION
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+const form = document.getElementById('contact-form');
+const errorElement = document.getElementById('error');
+
+form.addEventListener('submit', (e) => {
+    let messages = [];
+    if (name.value === '' || name.value == null) {
+        messages.push('Name is required');
+    }
+    if (email.value === '' || email.value == null) {
+        messages.push('Email is required');
+    }
+    if (subject.value === '' || subject.value == null) {
+        messages.push('Fill subject field');
+    }
+    if (message.value === '' || message.value == null) {
+        messages.push('Write message');
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
+    }
+});
+
+// function validateForm(){
+//     let name = document.forms["form"]["name"].value;
+//     if(name === ""){
+//         alert("Name field is empty");
+//         return false;
+//     }
+
+//     let email = document.forms["form"]["email"].value;
+//         alert("Email is required");
+//         return false;
+// }
